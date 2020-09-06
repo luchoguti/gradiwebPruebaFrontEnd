e027e1aaf6dfef0de71fb8fae4e00987af06f9c9
luis.torres <luchoguti9@gmail.com>
2020-09-05 21:18:12 -0500

    upload the necessary files to run the test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,6 +13,18 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $this->assertTrue (true);
+    }
+
+    public function testUserCreation()
+    {
+        $user = new User([
+            'name' => "Test User",
+            'email' => "test@mail.com",
+            'password' => bcrypt ("testpassword")
+        ]);
+
+        $this->assertEquals ('Test User', $user->name);
+
     }
 }
