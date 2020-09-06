8eee9a92b7f6f8117cee86dad6fdc70065d29f5a
luis.torres <luchoguti9@gmail.com>
2020-09-05 22:51:02 -0500

    changes are made test files

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,18 +13,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue (true);
-    }
-
-    public function testUserCreation()
-    {
-        $user = new User([
-            'name' => "Test User",
-            'email' => "test@mail.com",
-            'password' => bcrypt ("testpassword")
-        ]);
-
-        $this->assertEquals ('Test User', $user->name);
-
+        $this->assertTrue(true);
     }
 }
